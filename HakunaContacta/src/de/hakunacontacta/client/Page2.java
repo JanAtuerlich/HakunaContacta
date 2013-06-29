1d35266cd8a71ada4a1c2b410143e6e3b7d83de4
m.e.pruegel <mapruege@htwg-konstanz.de>
2013-06-29 15:26:33 +0200

    edited Page2.java

--- HakunaContacta/src/de/hakunacontacta/client/Page2.java
+++ HakunaContacta/src/de/hakunacontacta/client/Page2.java
@@ -1,11 +1,16 @@
 package de.hakunacontacta.client;
 
+//import com.google.api.client.util.Base64;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.http.client.Response;
 import com.google.gwt.user.client.History;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Hyperlink;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class Page2 extends Composite {
@@ -28,15 +33,38 @@ public class Page2 extends Composite {
 		page2.setBorderWidth(2);
 		page2.setPixelSize(150, 150);
 		HTML content = new HTML("This is page2. Click to move to back page1");
-		Button button = new Button("Back");
+		Button button = new Button("Create Export");
+		
+		final String dateiendung = "csv";
+		final String exportString = "Hallo Jan";
+		//final byte[] encoded = Base64.encodeBase64(exportString.getBytes()); 
+		//System.out.println(exportString);
+		//System.out.println(encoded);
+		
+
+
+		
+		
+		
 		button.addClickHandler(new ClickHandler() {
 			@Override
 			public void onClick(ClickEvent event) {
-				History.newItem("page1", true); 
+				//final HTML html = new HTML("<a download=\"Contactexport." + dateiendung + "\" href=data:application/" +dateiendung + ";base64," + encoded + ">Download</a>");
+				//final HTML html = new HTML("<a download=\"MyFile." + dateiendung + "\" href=data:application/vnd.ms-excel;base64,77u/Vm9ybmFtZTtOYWNobmFtZTtBZHJlc3NlO1RlbGVmb25udW1tZXI7DQpNYXJjZWw7UHLDvGdlbDtUb25hdXN0ci4gNDEgNzIxODkgVsO2aHJpbmdlbjsgMDE3NjYxNjc3NTAxOw0KTWF4OyBNdXN0ZXJtYW5uOyBNdXN0ZXJzdHIuIDEgNzg0NjcgS29uc3Rhbno7IDAxMjU2NDU0NTU7DQo=>Download</a>");
+				//page2.add(html);
+				
+				
+
+//				String uri ="<a download=\"MyFile.csv\" href=data:text/csv;charset=utf-8,\"test\">Download</a>";
+//				String uri ="href=data:text/csv;charset=utf-8,\"test\"";
+//				
+//				Window.open(uri, "TEST", "");
+//				
+//				History.newItem("page1", true); 
 			}
 		});
 		page2.add(content);
 		page2.add(button);
-		
+
 	}
 }
